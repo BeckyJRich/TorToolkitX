e8e40e6a880059d2aa603fa9b620f9beea049e86
amirulandalib <amirulandalib@gmail.com>
2021-07-24 09:46:39 +0000

    pylint: auto fixes

--- tortoolkit/consts/ExecVars.py
+++ tortoolkit/consts/ExecVars.py
@@ -37,9 +37,7 @@ except:
         REMAINING_STR = "▱"
 
         # DB URI for access
-        DATABASE_URL = (
-            "postgres://wbimpmtnnonomb:b82465d0aef8ac5f4848a086e383b12656050660b1659f14558e02c0aadf5057@ec2-35-168-145-180.compute-1.amazonaws.com:5432/d108q5ehr87d8g"
-        )
+        DATABASE_URL = "postgres://wbimpmtnnonomb:b82465d0aef8ac5f4848a086e383b12656050660b1659f14558e02c0aadf5057@ec2-35-168-145-180.compute-1.amazonaws.com:5432/d108q5ehr87d8g"
 
         # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
         # DATABASE_URL = "dbname=tortk user=postgres password=your-pass host=db port=5432"
